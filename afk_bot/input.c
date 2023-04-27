c1484b26bf497a19f1d7ecf6b9f6326724aac3b5
SilentSchnitzel <wtooyah278@gmail.com>
2023-04-26 23:35:13 -0400

    added code for the user input function

--- afk_bot/input.c
+++ afk_bot/input.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
-void user_input()
+int user_input()
 {
+    printf("Enter 1 if your OS is Windows or enter 2 in if you are using a distro of Linux. \n");
     
+    int os;
+    scanf("%d", &os);
+
+    printf("Enter in how many minutes you want the afk bot to run for");
+
+    int minutes;
+    scanf("%d", minutes);
+
+    printf("Enter in the amount of seconds that should be inbetween each automated mouse movement");
+    
+    int interval;
+    scanf("%d", interval);
+
+    return os, minutes, interval;
 }
 
--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+int user_input();
+
 int main()
 {
-    
+    int os, minutes, interval;
+    os, minutes, interval = user_input();
+
     return 0;
 }
